ad6e1655a130f8739943bb0e8519f9e9e91d28ba
Tyler Potts <tylerpotts@Tylers-MacBook-Pro-2.local>
2019-03-10 23:55:44 +0000

    Initial Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+	<nav>
+		<v-toolbar app dark>
+			<v-toolbar-side-icon v-on:click="isDrawerOpen = !isDrawerOpen" />
+			<v-toolbar-title>
+				<span class="font-weight-black">SOUNDBOARD</span><span class="font-weight-light">APP</span>
+			</v-toolbar-title>
+		</v-toolbar>
+
+		<v-navigation-drawer dark app v-model="isDrawerOpen">
+			<v-list>
+
+				<v-list-tile v-on:click="navigate('/')">
+					<v-list-tile-action>
+						<v-icon>home</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title>Home</v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+
+				<v-list-tile v-on:click="navigate('about')">
+					<v-list-tile-action>
+						<v-icon>info</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title>About</v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+
+				<v-list-tile v-on:click="navigate('support')">
+					<v-list-tile-action>
+						<v-icon>live_help</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title>Support</v-list-tile-title>
+					</v-list-tile-content>
+				</v-list-tile>
+
+			</v-list>
+		</v-navigation-drawer>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'Navbar',
+	data () {
+		return {
+			isDrawerOpen: false
+		}
+	},
+	methods: {
+		navigate: function (route) {
+			this.$router.push(route);
+		}
+	}
+}
+</script>
+
---
+++ src/components/Sounds.vue
@@ -0,0 +1,69 @@
+<template>
+	<v-container fluid grid-list-lg>
+		<v-layout wrap>
+			<v-flex xs4 v-for="(sound, index) in sounds" :key="index">
+				<v-card v-on:click="play(sound.sound)">
+					<v-card-text>
+						<v-icon color="yellow" x-large>play_arrow</v-icon>
+						<p>{{ sound.title }}</p>
+					</v-card-text>
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+export default {
+	name: 'Sounds',
+	data () {
+		return {
+			sounds: [
+				{
+					title: "Cow",
+					sound: "cow.mp3"
+				},
+				{
+					title: "Bell",
+					sound: "bell.mp3"
+				},
+				{
+					title: "Explosion",
+					sound: "explosion.mp3"
+				},
+				{
+					title: "Ambulance",
+					sound: "ambulance.mp3"
+				},
+				{
+					title: "Bullet",
+					sound: "bullet.mp3"
+				},
+				{
+					title: "Fizzle",
+					sound: "fizzle.mp3"
+				},
+				{
+					title: "Movie",
+					sound: "movie.mp3"
+				},
+				{
+					title: "Punch",
+					sound: "punch.mp3"
+				},
+				{
+					title: "Telephone",
+					sound: "telephone.mp3"
+				}
+			]
+		}
+	},
+	methods: {
+		play: function (name) {
+			let audio = new Audio(require('@/assets/sounds/' + name));
+			audio.play();
+		}
+	}
+}
+</script>
+
